1743481e59bf8586e25b475cc8546dd686ce164b
zamuna <zamuna16@gmail.com>
2017-04-11 12:20:33 -0500

    adding layout

---
+++ app/src/main/java/rms/manozct/resturantmanagement/activity/MenuActivity.java
@@ -0,0 +1,17 @@
+package rms.manozct.resturantmanagement.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import rms.manozct.resturantmanagement.R;
+
+/**
+ * Created by Zamuna on 4/11/2017.
+ */
+
+public class MenuActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+    }
+}
