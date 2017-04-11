d1095b911945364ba92d0dd8a83a13d3223f49d8
Tulasi Dahal Dahal <tdahal2012@gmail.com>
2017-04-11 18:01:02 -0500

    Added nav bar codes

--- app/src/main/java/rms/manozct/resturantmanagement/activity/EmployeeActivity.java
+++ app/src/main/java/rms/manozct/resturantmanagement/activity/EmployeeActivity.java
@@ -1,7 +1,14 @@
 package rms.manozct.resturantmanagement.activity;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,25 +16,38 @@ import android.widget.Toast;
 
 import rms.manozct.resturantmanagement.R;
 
-public class EmployeeActivity extends AppCompatActivity {
-    private EditText nameTxt;
-    private Button submitBtn;
+public class EmployeeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
+    private ActionBarDrawerToggle toggle;
+    private FragmentManager fragmentManager;
+    private boolean doubleBackToExitPressedOnce = false;
+
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee);
 
-        nameTxt = (EditText) findViewById(R.id.nameText);
-        submitBtn = (Button) findViewById(R.id.submitBtn);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        submitBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String name = nameTxt.getText().toString();
-                Toast.makeText(EmployeeActivity.this, "Hello "+name, Toast.LENGTH_SHORT).show();
-            }
-        });
+        fragmentManager = getSupportFragmentManager();
 
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
 
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        navigationView.setCheckedItem(R.id.nav_dashboard);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
