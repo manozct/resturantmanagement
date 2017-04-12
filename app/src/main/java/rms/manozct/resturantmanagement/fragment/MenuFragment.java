5a9e6a6ba7c27675d347c42f032178e05c1b3040
manoj chaudhary <ctmanoj55@gmail.com>
2017-04-11 23:46:08 -0500

    added InventoryFragment MenuFragment and SubMenuFragment

---
+++ app/src/main/java/rms/manozct/resturantmanagement/fragment/MenuFragment.java
@@ -0,0 +1,100 @@
+package rms.manozct.resturantmanagement.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import rms.manozct.resturantmanagement.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MenuFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MenuFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MenuFragment extends Fragment {
+  private EditText menuText;
+    private Button submitBtn;
+
+
+    private OnFragmentInteractionListener mListener;
+
+    public MenuFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view=inflater.inflate(R.layout.fragment_menu, container, false);
+        menuText=(EditText) view.findViewById(R.id.menuTxt);
+        submitBtn=(Button) view.findViewById(R.id.submitBtn);
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String menuName=menuText.getText().toString();
+                Toast.makeText(MenuFragment.this.getActivity(),"Menu:"+menuName,Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+}
---
+++ app/src/main/java/rms/manozct/resturantmanagement/fragment/SubMenuFragment.java
@@ -0,0 +1,98 @@
+package rms.manozct.resturantmanagement.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import rms.manozct.resturantmanagement.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link SubMenuFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link SubMenuFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SubMenuFragment extends Fragment {
+   private EditText subMenuText;
+   private Button submitBtn;
+
+
+
+    private OnFragmentInteractionListener mListener;
+
+    public SubMenuFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view=inflater.inflate(R.layout.fragment_sub_menu, container, false);
+        subMenuText=(EditText)view.findViewById(R.id.menuTxt);
+        submitBtn=(Button)view.findViewById(R.id.submitBtn);
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String subMenu=subMenuText.getText().toString();
+                Toast.makeText(SubMenuFragment.this.getActivity(), "Sub Menu:"+subMenu, Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
