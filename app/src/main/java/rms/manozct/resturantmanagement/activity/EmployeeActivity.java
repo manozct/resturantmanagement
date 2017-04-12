51c9f89fe1c9c2047f4f9e103a8868371fcd72f7
Tulasi Dahal Dahal <tdahal2012@gmail.com>
2017-04-11 21:26:34 -0500

    Added new fragment

--- app/src/main/java/rms/manozct/resturantmanagement/activity/EmployeeActivity.java
+++ app/src/main/java/rms/manozct/resturantmanagement/activity/EmployeeActivity.java
@@ -1,13 +1,20 @@
 package rms.manozct.resturantmanagement.activity;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -15,8 +22,9 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import rms.manozct.resturantmanagement.R;
+import rms.manozct.resturantmanagement.fragment.EmployeeFragment;
 
-public class EmployeeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class EmployeeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, EmployeeFragment.OnFragmentInteractionListener {
     private DrawerLayout drawer;
     private ActionBarDrawerToggle toggle;
     private FragmentManager fragmentManager;
@@ -47,7 +55,98 @@ public class EmployeeActivity extends AppCompatActivity implements NavigationVie
     }
 
     @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            int backCount = fragmentManager.getBackStackEntryCount();
+            System.out.println("back count:"+backCount);
+            if (backCount>0){
+                //getCurrentFragment(fragmentManager.getBackStackEntryAt(fragmentManager.getBackStackEntryCount() - 1).getName());
+                super.onBackPressed();
+            }else {
+                //Check if backBtn is already pressed.
+                if (doubleBackToExitPressedOnce) {
+                    exitApp();
+                }
+                this.doubleBackToExitPressedOnce = true;
+                Toast.makeText(this, "Press back again if you wish to exit", Toast.LENGTH_SHORT).show();
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        doubleBackToExitPressedOnce = false;
+                    }
+                }, 2000);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_search) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        return false;
+        changeLayout(item.getItemId());
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void changeLayout(int id){
+        switch (id) {
+            case R.id.nav_account:
+                replaceFragment(new EmployeeFragment());
+                break;
+            case R.id.nav_cart:
+                replaceFragment(new EmployeeFragment());
+                break;
+        }
+    }
+
+    public void replaceFragment(Fragment newFragment){
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.content_frame, newFragment);
+        String backStateName = newFragment.getClass().getName();
+        System.out.println("Fragment tag:"+backStateName);
+        fragmentTransaction.addToBackStack(backStateName);    //Add previous state to backstack
+        fragmentTransaction.commit();
+    }
+
+    /*@Override
+    public void setDrawerEnabled(boolean enabled) {
+        int lockMode = enabled ? DrawerLayout.LOCK_MODE_UNLOCKED :
+                DrawerLayout.LOCK_MODE_LOCKED_CLOSED;
+        drawer.setDrawerLockMode(lockMode);
+        toggle.setDrawerIndicatorEnabled(enabled);
+    }*/
+
+    private void exitApp(){
+        finish();
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
---
+++ app/src/main/java/rms/manozct/resturantmanagement/fragment/EmployeeFragment.java
@@ -0,0 +1,109 @@
+package rms.manozct.resturantmanagement.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import rms.manozct.resturantmanagement.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link EmployeeFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * * create an instance of this fragment.
+ */
+public class EmployeeFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    private EditText nameText;
+    private EditText addressText;
+    private EditText contactNoText;
+    private EditText ssnText;
+    private DatePicker dobDate;
+    private DatePicker hireDate;
+
+    private Button submitBtn;
+    public EmployeeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_employee, container, false);
+
+        nameText = (EditText) view.findViewById(R.id.nameTxt);
+        addressText = (EditText) view.findViewById(R.id.addressTxt);
+        contactNoText = (EditText) view.findViewById(R.id.contactTxt);
+        ssnText = (EditText) view.findViewById(R.id.snnTxt);
+
+        dobDate = (DatePicker) view.findViewById(R.id.dob);
+
+        submitBtn = (Button) view.findViewById(R.id.submitBtn);
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = nameText.getText().toString();
+                Toast.makeText(EmployeeFragment.this.getActivity(), "Hello "+name, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
