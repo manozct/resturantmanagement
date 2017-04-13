6f709497a9f4bd2813ed6176aa76e5d8577a0538
manoj chaudhary <ctmanoj55@gmail.com>
2017-04-12 22:34:16 -0500

    commit

--- app/src/main/java/rms/manozct/resturantmanagement/activity/EmployeeActivity.java
+++ app/src/main/java/rms/manozct/resturantmanagement/activity/EmployeeActivity.java
@@ -16,15 +16,11 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import rms.manozct.resturantmanagement.R;
 import rms.manozct.resturantmanagement.fragment.EmployeeFragment;
 import rms.manozct.resturantmanagement.fragment.MenuFragment;
-import rms.manozct.resturantmanagement.fragment.SubMenuFragment;
 
 public class EmployeeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, EmployeeFragment.OnFragmentInteractionListener,MenuFragment.OnFragmentInteractionListener {
     private DrawerLayout drawer;
