427558dad6b87f35cbfe52cd3bcbaaabb18732cc
Tulasi Dahal Dahal <tdahal2012@gmail.com>
2017-04-15 18:59:59 -0500

    Added SubMenu Adapter

--- app/src/main/java/rms/manozct/resturantmanagement/fragment/SubMenuListFragment.java
+++ app/src/main/java/rms/manozct/resturantmanagement/fragment/SubMenuListFragment.java
@@ -7,27 +7,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import rms.manozct.resturantmanagement.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link SubMenuFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.* create an instance of this fragment.
- */
-public class SubMenuFragment extends Fragment {
-   private EditText subMenuText;
-   private Button submitBtn;
-
-
-
+public class SubMenuListFragment extends Fragment {
     private OnFragmentInteractionListener mListener;
 
-    public SubMenuFragment() {
+    private int mColumnCount = 2;
+    public SubMenuListFragment() {
         // Required empty public constructor
     }
 
@@ -35,24 +22,13 @@ public class SubMenuFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view=inflater.inflate(R.layout.fragment_sub_menu, container, false);
-        subMenuText=(EditText)view.findViewById(R.id.menuTxt);
-        submitBtn=(Button)view.findViewById(R.id.submitBtn);
-        submitBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String subMenu=subMenuText.getText().toString();
-                Toast.makeText(SubMenuFragment.this.getActivity(), "Sub Menu:"+subMenu, Toast.LENGTH_SHORT).show();
-            }
-        });
-        return view;
+        return inflater.inflate(R.layout.fragment_sub_menu_list, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -79,16 +55,6 @@ public class SubMenuFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
